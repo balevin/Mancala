34d6cb0623a45c9d562bb89d705f8a98f4c9d435
Brad Levin <balevin@mit.edu>
2020-09-11 15:14:01 -0400

    added README

--- trainAndSee.py
+++ trainAndSee.py
@@ -19,10 +19,10 @@ pickle_in = open('thisGuysPrettyGood.pickle', 'rb')
 qvals = pickle.load(pickle_in)
 player1 = DiscreetQValuesPlayer(training=False, qValues=qvals)
 player1.setPerson(True)
-pickle_in = open('player2.pickle', 'rb')
-qvals2 = pickle.load(pickle_in)
-player2 = DiscreetQValuesPlayer(training=False, qValues=qvals2)
-player2.setPerson(False)
+# pickle_in = open('player2.pickle', 'rb')
+# qvals2 = pickle.load(pickle_in)
+# player2 = DiscreetQValuesPlayer(training=False, qValues=qvals2)
+# player2.setPerson(False)
 
 # game_number, p1_wins, p2_wins, draws = evaluate_players(player1, player2, games_per_battle=100000, num_battles=50)
 # player1.saveStateCounts()
@@ -52,21 +52,21 @@ while not board2.isOver():
         board2.makeMove(move)
         print(board2)
 
-board = Board()
-print(board)
-while not board.isOver():
-    if board.myTurn:
-        move = input('bottom: input which pile you would like to move: \n')
-        # player1.move(board2)
-        board.makeMove(move)
-        print(board)
-    else:
-        player2.move(board)
-        # move = input('top: input which pile you would like to move: \n')
-        # board.makeMove(move)
-        print(board)
+# board = Board()
+# print(board)
+# while not board.isOver():
+#     if board.myTurn:
+#         move = input('bottom: input which pile you would like to move: \n')
+#         # player1.move(board2)
+#         board.makeMove(move)
+#         print(board)
+#     else:
+#         player2.move(board)
+#         # move = input('top: input which pile you would like to move: \n')
+#         # board.makeMove(move)
+#         print(board)
 
-print(board)
+# print(board)
        
 board2 = Board()
 print(board2)
